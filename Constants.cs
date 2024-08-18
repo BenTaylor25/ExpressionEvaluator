f6609f0080cf9818a06dd5dfe69cbb863c93b2da
Ben Taylor <ben.taylor2573@gmail.com>
2024-08-18 13:23:24 +0100

    added infinite loop to CLI program

--- Constants.cs
+++ Constants.cs
@@ -5,4 +5,13 @@ internal static class Constants
     // Only not an explicit const so that my IDE doesn't complain
     // that there is unreachable code.
     public static readonly bool DEBUG_MODE = false;
+
+    public static readonly List<string> CLI_EXIT_KEYWORDS = new()
+    {
+        "exit",
+        "quit",
+        "close",
+        "x",
+        "q"
+    };
 }
--- Program.cs
+++ Program.cs
@@ -1,18 +1,27 @@
 ﻿using ExpressionEvaluator;
 
-Console.Write("Expr: ");
-string inputExpr = Console.ReadLine() ?? "";
-inputExpr = String.Concat(inputExpr.Where(c => !Char.IsWhiteSpace(c)));
-
-ExprParser parser = new(inputExpr);
-
-string value = parser.Parse();
-
-if (parser.State == ParserState.Succeeded)
+while (true)
 {
-    Console.WriteLine(value);
-}
-else
-{
-    Console.WriteLine("Expression is invalid.");
+    Console.Write("Expr: ");
+    string inputExpr = Console.ReadLine() ?? "";
+    inputExpr = String.Concat(inputExpr.Where(c => !Char.IsWhiteSpace(c)));
+
+    if (Constants.CLI_EXIT_KEYWORDS.Contains(inputExpr))
+    {
+        break;
+    }
+
+    ExprParser parser = new(inputExpr);
+
+    string value = parser.Parse();
+
+    if (parser.State == ParserState.Succeeded)
+    {
+        Console.WriteLine(value);
+    }
+    else
+    {
+        Console.WriteLine("Expression is invalid.");
+    }
+    Console.WriteLine();
 }
